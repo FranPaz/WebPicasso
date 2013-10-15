7a96663d79b9a3d9c58a3d2d8a42b8cf0d9cb430
ramons03 <ramons_03@hotmail.com>
2013-10-15 16:41:13 -0300

    Proyecto Inicial Picasso
    Este es el proyecto generado por defecto por el visual, con el turko
    agregamos:
    - paquete del foundation y los cambios necesarios para usarlo
    - paquete de MvcScaffolding
    -Agregamos las vistas de Empresa
    -Hicimos el modelo y Scaffolding de Sucursales
    -Hicimos 2 layout parciales "_Header" y "_Footer"
    -Agregue 2 .Css para hacer la barra de navegacion

---
+++ PicassoWeb/Controllers/SucursalController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PicassoWeb.Models;
+
+namespace PicassoWeb.Controllers
+{   
+    public class SucursalController : Controller
+    {
+        private PicassoWebContext context = new PicassoWebContext();
+
+        //
+        // GET: /Sucursal/
+
+        public ViewResult Index()
+        {
+            
+            return View(context.Sucursal.ToList());
+        }
+
+        //
+        // GET: /Sucursal/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Sucursal sucursal = context.Sucursal.Single(x => x.id == id);
+            return View(sucursal);
+        }
+
+        //
+        // GET: /Sucursal/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Sucursal/Create
+
+        [HttpPost]
+        public ActionResult Create(Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Sucursal.Add(sucursal);
+                context.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(sucursal);
+        }
+        
+        //
+        // GET: /Sucursal/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Sucursal sucursal = context.Sucursal.Single(x => x.id == id);
+            return View(sucursal);
+        }
+
+        //
+        // POST: /Sucursal/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(sucursal).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sucursal);
+        }
+
+        //
+        // GET: /Sucursal/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Sucursal sucursal = context.Sucursal.Single(x => x.id == id);
+            return View(sucursal);
+        }
+
+        //
+        // POST: /Sucursal/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sucursal sucursal = context.Sucursal.Single(x => x.id == id);
+            context.Sucursal.Remove(sucursal);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PicassoWeb/Models/Sucursal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PicassoWeb.Models
+{
+    public class Sucursal
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string nombre { get; set; }
+        public string dir { get; set; }
+        public string tel { get; set; }
+        public string mail { get; set; }
+        public string horarios { get; set; }
+        
+    }
+
+    
+
+}
